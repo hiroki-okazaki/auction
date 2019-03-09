a1f68d27af311c3a7784cfc5248842a58ff2e43b
hiroki-okazaki <1031okazaki@gmail.com>
2019-03-09 16:21:42 +0900

    index実装

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,10 @@
+class ItemsController < ApplicationController
+  def show
+  # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
+  # 取得したデータを@itemというインスタンス変数にセットする
+    @item = Item.find(params[:id])
+  end
+  def index
+    @items = Item.all
+  end
+end
